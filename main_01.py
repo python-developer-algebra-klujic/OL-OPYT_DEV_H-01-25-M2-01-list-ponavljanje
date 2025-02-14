f19c3842c543d182ecedfddac2ef73536f1f28ce
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-14 20:29:26 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -1,3 +1,4 @@
+import random
 
 
 numbers_1_to_50 = []
@@ -12,14 +13,22 @@ for number in range(1, 13): # od 1 do 12 -> prvi broj je ukljucen, a zadnji nije
     numbers_1_to_12.append(number)
 
 
-for _ in range(5):
-    number = numbers_1_to_50[15]
-    wining_combination.append(number)
+# for _ in range(5):
+while len(wining_combination) < 5:
+    random_number = random.randint(0, 49)
+    number = numbers_1_to_50[random_number]
+
+    if number not in wining_combination:
+        wining_combination.append(number)
 
 
-for _ in range(2):
-    number = numbers_1_to_12[15]
-    wining_combination.append(number)
+# for _ in range(2):
+while len(wining_combination) < 7:
+    random_number = random.randint(0, 11)
+    number = numbers_1_to_12[random_number]
+
+    if number not in wining_combination:
+        wining_combination.append(number)
 
 
 # ispisati izvucene brojeve
@@ -27,12 +36,20 @@ print()
 for i in range(7):
     if i == 6:
         print(f'{wining_combination[i]}')
+    elif i == 5:
+        print(f'{wining_combination[i]}', end=', ')
+    elif i == 4:
+        print(f'{wining_combination[i]}')
     else:
         print(f'{wining_combination[i]}', end=', ')
 
 print()
 for index, number in enumerate(wining_combination):
     if index == 6:
+        print(f'{number}')
+    elif index == 5:
+        print(f'{number}', end=', ')
+    elif index == 4:
         print(f'{number}')
     else:
         print(f'{number}', end=', ')
