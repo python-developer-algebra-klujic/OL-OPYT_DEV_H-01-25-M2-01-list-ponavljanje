aad2cfa29434f6ff30e46d92364c5e786d101437
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-14 19:32:03 +0100

    Update main_01.py

---
+++ main_01.py
@@ -0,0 +1,15 @@
+
+
+numbers_1_to_50 = []
+numbers_1_to_12 = []
+
+
+for number in range(1, 51): # od 1 do 50 -> prvi broj je ukljucen, a zadnji nije i korak je predefiniran na 1
+    numbers_1_to_50.append(number)
+
+for number in range(1, 13): # od 1 do 12 -> prvi broj je ukljucen, a zadnji nije i korak je predefiniran na 1
+    numbers_1_to_12.append(number)
+
+
+print(numbers_1_to_50)
+print(numbers_1_to_12)
