86c5ae4e7e20474d823ae62a0aa1efdcf6ecf80f
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-17 19:35:37 +0100

    Update main_03.py

--- main_03.py
+++ main_03.py
@@ -12,7 +12,7 @@ while True:
     movies.append([movie_id, movie])
     movie_id += 1
 
-    next_movie = input('Zelite li unijeti novi film? (dan/ne): ')
+    next_movie = input('Zelite li unijeti novi film? (da/ne): ')
     if next_movie.lower() != 'da':
         break
 
@@ -45,7 +45,7 @@ movies[2][1] => 'f'
 
 1.b. Napravite slican zadataka prvom, ali u ovoj verziji neka bude unos playliste.
         Nakon unosa liste pjesama, onda ispisite listu, ali "shuffled".
-'''
+
 
 import random as rnd
 
@@ -57,7 +57,7 @@ while True:
     song = input('Upisite naziv pjesme: ')
     playlist.append(song)
 
-    next_song = input('Zelite li dodati novu pjesmu? (dan/ne): ')
+    next_song = input('Zelite li dodati novu pjesmu? (da/ne): ')
     if next_song.lower() != 'da':
         break
 
@@ -70,7 +70,13 @@ for index, song in enumerate(playlist):
 
 
 
-'''
+
+
+
+
+
+
+
 2. Napišite program koji generira note akorda na osnovu početnog tona, odnosno note.
         POJAŠNJENJE
         Akord se sastoji od tri tona koji se mogu ponavljati.
@@ -98,3 +104,68 @@ for index, song in enumerate(playlist):
 
         3. Nakon toga pita korisnika zeli li generirati jos jedan akord.
 '''
+
+#           0    1     2     3    4    5    6     7    8     9    10    11
+# tones = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H']
+
+while True:
+    tones = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H']
+
+    for tone in tones:
+        print(tone, end=' ')
+    print()
+
+    chord_note = input('Upisite notu za koju zelite generirati akord: ')
+    chord_note = chord_note.upper()
+
+    # lista.extend(nova_lista) - metoda koja listu prosiruje s novom listom
+    #   tako da novu listu doda na kraj prve liste
+    tones.extend(tones)
+    chord_note_index = tones.index(chord_note)
+    chord_note_third = tones[chord_note_index + 3]
+    chord_note_fourth = tones[chord_note_index + 4]
+    chord_note_seventh = tones[chord_note_index + 7]
+
+    # Durski akord je pocetni ton pa 4 pa 7
+    print(f'{chord_note}: {chord_note}, {chord_note_fourth}, {chord_note_seventh}')
+
+
+    # Molski akord je pocetni ton pa 4 pa 7
+    print(f'{chord_note.lower()} mol: {chord_note}, {chord_note_third}, {chord_note_seventh}')
+    print()
+    tones.clear()
+
+    next_tone = input('Zelite li generirati novi akord? (da/ne): ')
+    if next_tone.lower() != 'da':
+        break
+
+
+
+
+
+
+
+
+
+
+# Mario Vojić
+
+lista_nota = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "H"]
+
+slovo_tona = input ('Unesite slovo tona: ')
+
+for i in range(len(lista_nota)):
+    if slovo_tona == lista_nota[i]:
+        indeks = i
+        break
+
+
+dur_ton_I = ((indeks + 4) % 12)
+dur_ton_II = ((indeks + 7) % 12)
+mol_ton_I = ((indeks + 3) % 12)
+mol_ton_II = ((indeks + 7) % 12)
+
+
+print()
+print (f'{lista_nota[indeks]} ({lista_nota[indeks]} dur): {lista_nota[indeks]} {lista_nota[dur_ton_I]} {lista_nota[dur_ton_II]} ')
+print (f'{lista_nota[indeks]} ({lista_nota[indeks]} mol): {lista_nota[indeks]} {lista_nota[mol_ton_I]} {lista_nota[mol_ton_II]} ') 
